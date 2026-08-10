42eb477bb133bf8b586529150d19be1afcc6ab2b
Heiner Schmidt <heiner.schmidt@gmail.com>
2026-08-10 20:50:20 +0200

    added QuadGridIcons class

---
+++ Runtime/QuadGridIcons.cs
@@ -0,0 +1,16 @@
+namespace QuadUI
+{
+    public static class QuadGridIcons
+    {
+        public static QuadGrid TheLetterQ = new QuadGrid(7, 7, new int[49]
+        {
+                0,0,1,1,1,0,0,
+                0,1,0,0,0,1,0,
+                1,0,0,0,0,0,1,
+                1,0,0,1,0,0,1,
+                1,0,0,0,1,0,1,
+                0,1,0,0,0,1,0,
+                0,0,1,1,1,0,1,
+        });
+    }
+}
